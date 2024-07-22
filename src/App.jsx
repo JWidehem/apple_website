dcc78b89a72a0176627b99dd3b06d44ef6ceaafb
Widehem Jimmy <jimmywidehem@gmail.com>
2024-07-22 23:06:09 +0200

    Update .gitignore and package.json, configure Sentry for error tracking

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,11 @@ import Highlights from "./components/Highlights";
 import Navbar from "./components/Navbar";
 import Model from "./components/Model";
 
+import * as Sentry from "@sentry/react";
+
 const App = () => {
+  return <button onClick={() => methodDoesNotExist()}>Break the world</button>;
+
   return (
     <main className="bg-black">
       <Navbar />
@@ -14,4 +18,4 @@ const App = () => {
   );
 };
 
-export default App;
+export default Sentry.withProfiler(App);
